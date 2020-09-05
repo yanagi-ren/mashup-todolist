5c81d0bb61aad6d489791c7f695bc00d4d56b1e1
Liu8 <renxiang@Liu8ui-Macmini.local>
2020-09-05 22:35:05 +0900

    complete

--- src/components/TodoCreate.js
+++ src/components/TodoCreate.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styled, { css } from 'styled-components';
 import { MdAdd } from 'react-icons/md';
+import { useTodoDispatch, useTodoNextId } from '../TodoContext';
 
 const CircleButton = styled.button`
     background: #38d9a9;
@@ -51,7 +52,7 @@ const InsertFormPositioner = styled.div`
     position: absolute;
 `;
 
-const InsertForm = styled.div`
+const InsertForm = styled.form`
     background: #f8f9fa;
     padding: 32px;
     padding-bottom: 72px;
@@ -72,18 +73,47 @@ const Input = styled.input`
 
 function TodoCreate() {
     const [open, setOpen] = useState(false);
+    const [value, setValue] = useState('');
+  
+    const dispatch = useTodoDispatch();
+    const nextId = useTodoNextId();
+  
     const onToggle = () => setOpen(!open);
+    const onChange = e => setValue(e.target.value);
+    const onSubmit = e => {
+      e.preventDefault(); // 새로고침 방지
+      dispatch({
+        type: 'CREATE',
+        todo: {
+          id: nextId.current,
+          text: value,
+          done: false
+        }
+      });
+      setValue('');
+      setOpen(false);
+      nextId.current += 1;
+    };   
 
     return (
         <>
-        {open && <InsertFormPositioner><InsertForm>
-                <Input placeholder="할 일을 입력 후, Enter 를 누르세요" autoFocus />
-            </InsertForm></InsertFormPositioner>}
-        <CircleButton onClick={onToggle} open={open}>
+          {open && (
+            <InsertFormPositioner>
+              <InsertForm onSubmit={onSubmit}>
+                <Input
+                  autoFocus
+                  placeholder="할 일을 입력 후, Enter 를 누르세요"
+                  onChange={onChange}
+                  value={value}
+                />
+              </InsertForm>
+            </InsertFormPositioner>
+          )}
+          <CircleButton onClick={onToggle} open={open}>
             <MdAdd />
-        </CircleButton>
+          </CircleButton>
         </>
-    );
-}
+      );
+    }
 
-export default TodoCreate;
+export default React.memo(TodoCreate);
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -11,13 +11,20 @@ const TodoListBlock = styled.div`
 `;
 
 function TodoList() {
-    const state = useTodoState();
-    return <TodoListBlock>
-        <TodoItem text="프로젝트 생성하기" done={true} />
-        <TodoItem text="컴포넌트 스타일링 하기" done={true} />
-        <TodoItem text="Context 만들기" done={false} />
-        <TodoItem text="기능 구현하기" done={false} />
-    </TodoListBlock>;
+    const todos = useTodoState();
+    return (
+    <TodoListBlock>
+        {todos.map(
+            todo => (
+            <TodoItem
+                key={todo.id}
+                id={todo.id}
+                text={todo.text}
+                done={todo.done}
+            />
+            ))}    
+    </TodoListBlock>
+    );
 }
 
 export default TodoList;
